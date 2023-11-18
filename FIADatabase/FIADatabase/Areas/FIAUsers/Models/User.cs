8aad1d565541e1080d5b8c498f1cac5a7b146639
Eric Tashji <99293073+etashji@users.noreply.github.com>
2023-11-18 17:04:29 -0500

    Completed Users

---
+++ FIADatabase/FIADatabase/Areas/FIAUsers/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace FIADatabase.Areas.FIAUsers.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        [RegularExpression(@"^\S*$", ErrorMessage = "Username may not contain spaces")]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Username must be between 6 and 30 characters.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^\S*$", ErrorMessage = "Username may not contain spaces")]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 16 characters.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "You must confirm your password.")]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        public string Salt { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
+    
---
+++ FIADatabase/FIADatabase/Contexts/FIAUsersContext.cs
@@ -0,0 +1,20 @@
+﻿using FIADatabase.Areas.FIAUsers.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FIADatabase.Contexts
+{
+    public class FIAUsersContext : DbContext
+    {
+        public FIAUsersContext() : base("UsersConnection") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
+        public DbSet<User> Users { get; set; }  
+    }
+}
