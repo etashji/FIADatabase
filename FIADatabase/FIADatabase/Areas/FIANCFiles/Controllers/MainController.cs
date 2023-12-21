c9a45c58fb7c590200517605d30250f1aeb4321a
Eric Tashji <99293073+etashji@users.noreply.github.com>
2023-12-20 22:16:42 -0500

    Completed Misc

---
+++ FIADatabase/FIADatabase/Areas/FIANCFiles/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIADatabase.Areas.FIANCFiles.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: FIANCFiles/Main
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult NCIndex()
+        {
+            return View();
+        }
+    }
+}
